be9fbe04894bd6179d4766451b6876aef94ddbb2
Harold Jean <haroldjean38@yahoo.com>
2022-11-18 12:50:32 -0500

    placed fetch in fetch folder

---
+++ fetch-utils.js
@@ -0,0 +1,8 @@
+// Set Up Supbase Key and Url
+const SUPABASE_URL = 'https://dssscdbblqlagvgvvtkc.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzc3NjZGJibHFsYWd2Z3Z2dGtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwMTYsImV4cCI6MTk4MzY4NDAxNn0.ZLiT0DIvTuegM8zzRIvSgVzLLFRk-1PwYGXz7BhqKk4';
+
+const client = supabase.createclient(SUPABASE_URL, SUPABASE_KEY);
+
+
+export async function fetch
