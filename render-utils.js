6545958189fe238d8fd95d114605c07f468cd9eb
Harold Jean <haroldjean38@yahoo.com>
2022-11-22 12:41:46 -0500

    changed title size to h2

--- render-utils.js
+++ render-utils.js
@@ -1,51 +1,51 @@
 export function renderAnimals(animals) {
     const div = document.createElement('div');
 
-    const h2 = document.createElement('h2');
-    h2.textContent = animals.name;
+    const h4 = document.createElement('h4');
+    h4.textContent = animals.name;
 
     const p = document.createElement('p');
     p.textContent = animals.name;
 
-    div.append(h2, p);
+    div.append(h4, p);
     return div;
 }
 
 export function renderBitcoins(bitcoins) {
     const div = document.createElement('div');
 
-    const h2 = document.createElement('h2');
-    h2.textContent = bitcoins.address;
+    const h4 = document.createElement('h4');
+    h4.textContent = bitcoins.address;
 
     const p = document.createElement('p');
     p.textContent = bitcoins.address;
 
-    div.append(h2, p);
+    div.append(h4, p);
     return div;
 }
 
 export function renderSsns(ssns) {
     const div = document.createElement('div');
 
-    const h2 = document.createElement('h2');
-    h2.textContent = ssns.ssns;
+    const h4 = document.createElement('h4');
+    h4.textContent = ssns.ssns;
 
     const p = document.createElement('p');
     p.textContent = ssns.ssns;
 
-    div.append(h2, p);
+    div.append(h4, p);
     return div;
 }
 
 export function renderCreditCards(cards) {
     const div = document.createElement('div');
 
-    const h2 = document.createElement('h2');
-    h2.textContent = cards.credit_card;
+    const h4 = document.createElement('h4');
+    h4.textContent = cards.credit_card;
 
     const p = document.createElement('p');
     p.textContent = cards.credit_card;
 
-    div.append(h2, p);
+    div.append(h4, p);
     return div;
 }
